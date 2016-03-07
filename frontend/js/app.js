0f23b8dab7d85f2db71621d3b6cd6ebb3a48b1b4
intheon <allobon@gmail.com>
2016-03-07 14:49:06 +0000

    styling

---
+++ frontend/js/app.js
@@ -0,0 +1,5 @@
+$(".hideUpVote").click(() => {
+
+	$(".hideUpVote").addClass("animated");
+
+});
--- frontend/js/directives.js
+++ frontend/js/directives.js
@@ -1,7 +1,7 @@
 app.directive("upvoteButton", function () {
         return {
             restrict: 'E',
-            template: '<div class="upvote"></div>',
+            template: "<div class='svg-div'><svg width='32' height='32' class='hideUpVote'><line x1='2' y1='32' x2='16' y2='8'/><line x1='30' y1='32' x2='16' y2='8'/></svg></div>",
             replace: true
         };
     });
