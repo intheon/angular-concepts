fc4a1ffecf78b27444d7de05d76042e762e6f953
intheon <allobon@gmail.com>
2016-04-24 22:37:24 +0100

    refine dem styles

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -159,14 +159,14 @@ app.controller("SearchCtrl", ($scope, $http, $rootScope, NgMap) => {
 });
 
 // Controller for Google maps presentation, marker and infoWindow logic
-app.controller("MapCtrl", ($scope, $http, $rootScope, NgMap, Upload) => {
+app.controller("MapCtrl", ($scope, $http, $rootScope, NgMap, Upload, miscHelpFunctionsService) => {
 
 	// Namespace
 	const inst = this;
 
 	// Apply some custom css
-	$scope.styles = [{"featureType":"water","elementType":"all","stylers":[{"hue":"#76aee3"},{"saturation":38},{"lightness":-11},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"hue":"#8dc749"},{"saturation":-47},{"lightness":-17},{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"hue":"#c6e3a4"},{"saturation":17},{"lightness":-2},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"hue":"#cccccc"},{"saturation":-100},{"lightness":13},{"visibility":"on"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"hue":"#5f5855"},{"saturation":6},{"lightness":-31},{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[]}]
-
+	//$scope.styles = [{"featureType":"water","elementType":"all","stylers":[{"hue":"#76aee3"},{"saturation":38},{"lightness":-11},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"hue":"#8dc749"},{"saturation":-47},{"lightness":-17},{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"hue":"#c6e3a4"},{"saturation":17},{"lightness":-2},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"hue":"#cccccc"},{"saturation":-100},{"lightness":13},{"visibility":"on"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"hue":"#5f5855"},{"saturation":6},{"lightness":-31},{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[]}]
+	$scope.styles = [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
 	// This is fired after the server has done it's thing
 	$rootScope.$on("runMapCtrl", () => {
 
@@ -248,10 +248,65 @@ app.controller("MapCtrl", ($scope, $http, $rootScope, NgMap, Upload) => {
 
 			}
 
+			$scope.isEditing = false;
+
+			$scope.toggleEditing = () => {
+
+				if (!$scope.isEditing)
+				{
+					$scope.isEditing = true;
+
+					miscHelpFunctionsService.displayErrorMessage("Click or tap on the map to add a new park!");
+
+					$(".toggleControl").removeClass("white");
+					$(".toggleControl").addClass("green");
+					$(".toggleControl").text("Stop Editing")
+				}
+				else if ($scope.isEditing)
+				{
+					$scope.isEditing = false;
+
+					$(".toggleControl").removeClass("green");
+					$(".toggleControl").addClass("white");
+					$(".toggleControl").text("ADD | EDIT")
+
+				}
+
+			}
+
+			$scope.addTag = (tagName) => {
+
+				console.log(tagName);
+
+				$(this).addClass("active-chip");
+				
+			}
+
+
 
 			// NOW THE FUN STUFF
 			// Make the map clickable
 			google.maps.event.addListener(inst.map, "click", (event) => {
+
+				if (!$scope.isEditing) return false;
+
+				setTimeout(() => {
+
+					const goo  = $(".gm-style-iw");
+					const background1 = $(goo).prev().children(":nth-child(2)");
+					const background2 = $(goo).prev().children(":nth-child(4)");
+					const pin1 = $(goo).prev().children(":nth-child(3)").children(":first-child");
+					const pin2 = $(goo).prev().children(":nth-child(3)").children(":last-child");
+
+					$(background1).css({"background":"transparent"});
+					$(background1).css({"box-shadow":"none"});
+					$(background2).css({"background":"transparent"});
+					$(pin1).css({"top":"-12px"});
+					$(pin2).css({"top":"-12px"});
+
+				},25);
+
+
 
 				// get the latLng from precisely where the user clicked
 				const clickedLocation = [{
